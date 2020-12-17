fbdb56822bdc0ce494d0fceeb25da3185f17d9a3
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2020-12-17 21:31:24 +0100

    Fixing composer service base url.

--- Unite.Web/Client/src/settings.js
+++ Unite.Web/Client/src/settings.js
@@ -1,4 +1,4 @@
 export default {
-    //baseUrl: "http://composer.unite/api",
-    baseUrl: "http://localhost:5010/api"
+    baseUrl: "http://composer.unite/api",
+    // baseUrl: "http://localhost:5010/api"
 }
