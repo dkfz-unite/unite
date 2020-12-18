df8f76ba2af5574f25d36eff38fce042f31d11f8
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2020-12-18 11:08:20 +0100

    Testing docker - localhost connection.

--- Unite.Web/Client/src/settings.js
+++ Unite.Web/Client/src/settings.js
@@ -1,4 +1,4 @@
 export default {
-    baseUrl: "http://composer.unite:80/api",
-    // baseUrl: "http://localhost:5010/api"
+    // baseUrl: "http://composer.unite:80/api",
+    baseUrl: "http://localhost:5010/api"
 }
