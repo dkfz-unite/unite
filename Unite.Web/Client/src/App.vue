8ba4901fa1a268f7ea2ac75b5730145c95a81e04
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2021-02-17 17:48:40 +0100

    Changing logo.

--- Unite.Web/Client/src/App.vue
+++ Unite.Web/Client/src/App.vue
@@ -4,7 +4,8 @@
     <q-header bordered class="bg-blue-7 text-white">
       <q-toolbar>
         <q-toolbar-title>
-          <div>DKFZ.UNITE</div>
+          <!-- <div>DKFZ.UNITE</div> -->
+          <img src="logo-white.png" width="250">
           <!-- <q-btn :to="{ name: 'home' }" class="text-bold" flat dense>DKFZ.UNITE</q-btn> -->
         </q-toolbar-title>
 
