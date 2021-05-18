2f7ce3cac8c1037bc8be6bde7e2f45ac63534c07
Nicolai Sandmann <Nicolai.Sandmann@student.uni-augsburg.de>
2021-05-18 18:54:27 +0200

    UND-250: - added the oncogrid legend - added the reset grid button - added tooltip when hovering over oncogrid data - changed oncogrid version

--- Unite.Web/Client/src/components/oncogrid/OncoGrid.vue
+++ Unite.Web/Client/src/components/oncogrid/OncoGrid.vue
@@ -1,10 +1,21 @@
 ﻿<template>
   <div>
     <div v-show="this.isOncoRendered">
-      <button @click="toggleGridLines()">Grid</button>
-      <button @click="toggleCrosshair()">Crosshair Mode</button>
+      <div class="onco-toolbar">
+        <div>
+          <q-btn icon="las la-border-all" @click="toggleGridLines()"></q-btn>
+          <q-btn icon="las la-crosshairs" @click="toggleCrosshair()"></q-btn>
+          <q-btn icon="las la-undo-alt" @click="reloadGrid()"></q-btn>
+        </div>
+        <div class="onco-legend-container">
+          <div class="onco-legend-square" v-bind:style="{backgroundColor:color}" v-for="(color, key) in colorMap">
+            <div class="onco-legend-text">
+              {{ key }}
+            </div>
+          </div>
+        </div>
+      </div>
       <div id="oncoGrid">
-
       </div>
     </div>
     <div v-if="!this.isOncoRendered">
@@ -16,18 +27,34 @@
     </div>
   </div>
 </template>
-
 <script>
 import OncoGrid from "oncogrid";
+
+let colorMap = {};
+colorMap['mutation'] = {
+  missense_variant: '#ff9b6c',
+  frameshift_variant: '#57dba4',
+  stop_gained: '#af57db',
+  start_lost: '#ff2323',
+  stop_lost: '#d3ec00',
+  initiator_codon_variant: '#5abaff'
+};
 
 let oncoGrid;
 
 export default {
   name: 'oncogrid',
   data() {
-    return {isOncoRendered: false}
+    return {
+      colorMap: colorMap['mutation'],
+      isOncoRendered: false
+    }
   },
-  methods:{
+  methods: {
+    reloadGrid() {
+      this.oncoGrid.reload();
+    },
+
     toggleCrosshair() {
       this.oncoGrid.toggleCrosshair();
     },
@@ -60,15 +87,45 @@ export default {
 
     var observations = [
       {"id": "MU1", "donorId": "DO1", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU11", "donorId": "DO1", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"},
+      {
+        "id": "MU11",
+        "donorId": "DO1",
+        "geneId": "ENSG00000157764",
+        "type": "mutation",
+        "consequence": "missense_variant"
+      },
       {"id": "MU2", "donorId": "DO1", "geneId": "ENSG00000141510", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU3", "donorId": "DO2", "geneId": "ENSG00000141510", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU4", "donorId": "DO3", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU5", "donorId": "DO4", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"},
+      {
+        "id": "MU3",
+        "donorId": "DO2",
+        "geneId": "ENSG00000141510",
+        "type": "mutation",
+        "consequence": "missense_variant"
+      },
+      {
+        "id": "MU4",
+        "donorId": "DO3",
+        "geneId": "ENSG00000157764",
+        "type": "mutation",
+        "consequence": "frameshift_variant"
+      },
+      {
+        "id": "MU5",
+        "donorId": "DO4",
+        "geneId": "ENSG00000157764",
+        "type": "mutation",
+        "consequence": "frameshift_variant"
+      },
       {"id": "MU6", "donorId": "DO4", "geneId": "ENSG00000164796", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU7", "donorId": "DO5", "geneId": "ENSG00000155657", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU8", "donorId": "DO5", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"},
-      {"id": "MU9", "donorId": "DO6", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "start_lost"}
+      {
+        "id": "MU7",
+        "donorId": "DO5",
+        "geneId": "ENSG00000155657",
+        "type": "mutation",
+        "consequence": "initiator_codon_variant"
+      },
+      {"id": "MU8", "donorId": "DO5", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "stop_gained"},
+      {"id": "MU9", "donorId": "DO6", "geneId": "ENSG00000157764", "type": "mutation", "consequence": "stop_gained"}
     ];
 
     var donorOpacity = function (d) {
@@ -123,23 +180,11 @@ export default {
       {'name': 'Foobar', 'fieldName': 'foobar', 'type': 'bool', 'group': 'Data', 'sort': sortBool}
     ];
 
-    var colorMap = {}
-    colorMap['mutation'] = {
-      'missense_variant': '#ff9b6c',
-      'frameshift_variant': '#57dba4',
-      'stop_gained': '#af57db',
-      'start_lost': '#ff2323',
-      'stop_lost': '#d3ec00',
-      'initiator_codon_variant': '#5abaff'
-    };
-
     var params = {
       element: '#oncoGrid',
       donors: donors,
       genes: genes,
       observations: observations,
-      height: 450,
-      width: 600,
       heatMap: true,
       trackHeight: 20,
       trackLegendLabel: '<i>?</i>',
@@ -152,13 +197,13 @@ export default {
 
     this.oncoGrid = new OncoGrid(params);
     this.oncoGrid.render();
-    //ToODO: maybe use oncoGrid != null instead of boolean property
+    //TODO: maybe use oncoGrid != null instead of boolean property
     this.isOncoRendered = true
   }
 };
 </script>
 
-<style scoped>
+<style>
 #oncoGrid {
   cursor: pointer;
 }
@@ -240,4 +285,47 @@ export default {
   stroke: green;
   stroke-width: 1;
 }
+
+.og-tooltip-oncogrid {
+  font-weight: 300;
+  position: absolute;
+  z-index: 100001;
+  display: block;
+  visibility: visible;
+  font-size: .9rem;
+  pointer-events: none;
+  line-height: 1.6rem;
+  border: 1px solid #B3B3B3;
+  background-color: #fff;
+  border-radius: 3px;
+  -webkit-filter: drop-shadow(0 1px 3px hsla(0, 0%, 71%, .43));
+  box-shadow: 0 1px 3px hsl(0, 71%, 43%);
+  -moz-box-shadow: 0 1px 3px hsla(0, 0%, 71%, .43);
+  filter: drop-shadow(0 1px 3px rgba(180, 180, 180, .43));
+  padding: 5px;
+}
+
+.onco-legend-container {
+  flex: 1 1 0;
+  background: #e6e6e6;
+  margin-left: 50px;
+}
+
+.onco-legend-square {
+  display: inline-block;
+  margin: 5px;
+}
+
+.onco-legend-text {
+  padding: 5px;
+  background: #e6e6e6;
+  margin-left: 30px;
+  width: 150px;
+}
+
+.onco-toolbar {
+  display: flex;
+  flex-direction: row;
+}
+
 </style>
