fca92f7ef9f8b765621c34a0a47802b51988ebcc
DGitRich <daniel.rich@gmx.net>
2023-05-10 17:39:24 +0200

    Update EnvironmentConfig

--- Unite.Web/Configuration/EnvironmentConfig.cs
+++ Unite.Web/Configuration/EnvironmentConfig.cs
@@ -2,6 +2,7 @@
 
 public static class EnvironmentConfig
 {
+    public static string IdentityHost => Environment.GetEnvironmentVariable("UNITE_IDENTITY_HOST");
     public static string ComposerHost => Environment.GetEnvironmentVariable("UNITE_COMPOSER_HOST");
     public static string DonorsFeedHost => Environment.GetEnvironmentVariable("UNITE_DONORS_FEED_HOST");
     public static string SpecimensFeedHost => Environment.GetEnvironmentVariable("UNITE_SPECIMENS_FEED_HOST");
