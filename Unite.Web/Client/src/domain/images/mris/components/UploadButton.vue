71daddd987531b2afa59ad54bfb9852a27ff8688
DGitRich <daniel.rich@gmx.net>
2023-10-09 17:02:30 +0200

    Create images UploadButton

---
+++ Unite.Web/Client/src/domain/images/mris/components/UploadButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <u-upload-dialog
+    v-model="dialog"
+    :subject="'Images'"
+    :templatePathJson="'/templates/images.jsonc'"
+    :templatePathTsv="'/templates/images.tsv'"
+    :uploadMethod="uploadImages"
+  />
+
+  <q-btn
+    label="Upload"
+    title="Upload data"
+    icon="las la-file-upload"
+    color="primary"
+    @click="onUploadImages"
+    v-close-popup
+    dense flat no-caps>
+  </q-btn>
+</template>
+
+<script>
+import ImagesApi from '../api/index';
+import UUploadDialog from '@/domain/_shared/components/upload/UploadDialog.vue';
+
+export default {
+  components: {
+    UUploadDialog,
+  },
+
+  setup() {
+    return {
+      imagesApi: new ImagesApi(),
+    };
+  },
+
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+
+  methods: {
+    async onUploadImages() {
+      this.dialog = true;
+    },
+
+    async uploadImages(data, format) {
+      return await this.imagesApi.uploadImages(data, format);
+    },
+  }
+}
+</script>
