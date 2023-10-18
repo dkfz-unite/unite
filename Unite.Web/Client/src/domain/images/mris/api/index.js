bfd46f0fbd8fd78fee5e6c368f9c5abd30949661
DGitRich <daniel.rich@gmx.net>
2023-10-18 13:12:43 +0200

    Update mris api index

--- Unite.Web/Client/src/domain/images/mris/api/index.js
+++ Unite.Web/Client/src/domain/images/mris/api/index.js
@@ -28,7 +28,7 @@ export default class ImagesApi extends DomainApi {
   async uploadImages(data, format = formats.json.name) {
     validateFormat(format);
 
-    const url = `${this.feedUrl}${formats[format].path ?? ""}`;
+    const url = `${this.feedUrl}/images${formats[format].path ?? ""}`;
     const body = data;
     const config = { headers: formats[format].headers };
 
