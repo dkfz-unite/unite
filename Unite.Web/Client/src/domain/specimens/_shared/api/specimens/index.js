a1952746f9cd4ea4532f0ce15e6942e32702c45d
DGitRich <daniel.rich@gmx.net>
2024-01-08 14:13:12 +0100

    Update specimens shared api

--- Unite.Web/Client/src/domain/specimens/_shared/api/specimens/index.js
+++ Unite.Web/Client/src/domain/specimens/_shared/api/specimens/index.js
@@ -6,6 +6,12 @@ const formats = {
   tsv: { name: "tsv", path: "/tsv", headers: { "Content-Type": "text/tab-separated-values" } },
 };
 
+function validateFormat(format) {
+  if (!formats[format]) {
+    throw new Error(`Invalid format: ${format}`);
+  }
+}
+
 export default class SpecimensApi extends DomainApi {
   feedUrl = `${settings.urls.specimens}`;
 
@@ -27,12 +33,41 @@ export default class SpecimensApi extends DomainApi {
   }
 
   /**
-   * Uploads tissues data.
+   * Uploads drugs data.
+   * @param {Object} data data to upload.
+   * @param {"json"|"tsv"} [format] data format (default: "json").
+   * @returns {Promise<Object>} A promise that resolves with the upload results.
+   */
+  async uploadDrugs(data, format = formats.json.name) {
+    validateFormat(format);
+
+    const url = `${this.feedUrl}/drugs${formats[format].path ?? ""}`;
+    const body = data;
+    const config = { headers: formats[format].headers };
+
+    return this.client.post(url, body, config);
+  }
+
+  /**
+   * Uploads tissues data in tsv format.
    * @param {Object} data data to upload.
    * @returns {Promise<Object>} A promise that resolves with the upload results.
    */
   async uploadTissues(data) {
     const url = `${this.feedUrl}/tissues/tsv`;
+    const body = data;
+    const config = { headers: formats.tsv.headers };
+
+    return this.client.post(url, body, config);
+  }
+
+  /**
+   * Uploads cells data in tsv format.
+   * @param {Object} data data to upload.
+   * @returns {Promise<Object>} A promise that resolves with the upload results.
+   */
+  async uploadCells(data) {
+    const url = `${this.feedUrl}/cells/tsv`;
     const body = data;
     const config = { headers: formats.tsv.headers };
 
