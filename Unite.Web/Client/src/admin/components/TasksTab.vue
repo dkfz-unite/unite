d0ad47bd04c444c9cbc5092b4469b2e9939c607f
vulasau <valiantsin.ulasau@gmail.com>
2024-09-13 17:55:29 +0200

    Updated task names.

--- Unite.Web/Client/src/admin/components/TasksTab.vue
+++ Unite.Web/Client/src/admin/components/TasksTab.vue
@@ -12,10 +12,10 @@
             <div class="row items-center q-gutter-x-sm">
               <div class="u-text-key">Total: {{ general.submission }}</div>
               <template v-if="submission">
-                <div><q-badge title="Simple Somatic Mutations (SSM)" :color="getColor(submission.SSM)" text-color="black" label="SSM">: {{ submission.SSM }}</q-badge></div>
-                <div><q-badge title="Copy Number Variants (CNV)" :color="getColor(submission.CNV)" text-color="black" label="CNV">: {{ submission.CNV }}</q-badge></div>
-                <div><q-badge title="Structural Variants (SV)" :color="getColor(submission.SV)" text-color="black" label="SV">: {{ submission.SV }}</q-badge></div>
-                <div><q-badge title="Bulk RNA (BGE)" :color="getColor(submission.TEX)" text-color="black" label="BGE">: {{ submission.BGE }}</q-badge></div>
+                <div><q-badge title="DNA - Simple Somatic Mutations (SSM)" :color="getColor(submission['dna-ssm'])" text-color="black" label="DNA-SSM">: {{ submission["dna-ssm"] }}</q-badge></div>
+                <div><q-badge title="DNA - Copy Number Variants (CNV)" :color="getColor(submission['dna-cnv'])" text-color="black" label="DNA-CNV">: {{ submission["dna-cnv"] }}</q-badge></div>
+                <div><q-badge title="DNA - Structural Variants (SV)" :color="getColor(submission['dna-sv'])" text-color="black" label="DNA-SV">: {{ submission["dna-sv"] }}</q-badge></div>
+                <div><q-badge title="RNA - Bulk gene expressions" :color="getColor(submission['rna-exp'])" text-color="black" label="RNA-EXP">: {{ submission["rna-exp"] }}</q-badge></div>
               </template>
             </div>
           </td>
@@ -26,9 +26,9 @@
             <template v-if="annotation">
               <div class="row items-center q-gutter-x-sm">
                 <div class="u-text-key">Total: {{ general.annotation }}</div>
-                <div><q-badge title="Simple Somatic Mutations (SSM)" :color="getColor(annotation.SSM)" text-color="black" label="SSM">: {{ annotation.SSM }}</q-badge></div>
-                <div><q-badge title="Copy Number Variants (CNV)" :color="getColor(annotation.CNV)" text-color="black" label="CNV">: {{ annotation.CNV }}</q-badge></div>
-                <div><q-badge title="Structural Variants (SV)" :color="getColor(annotation.SV)" text-color="black" label="SV">: {{ annotation.SV }}</q-badge></div>
+                <div><q-badge title="DNA - Simple Somatic Mutations (SSM)" :color="getColor(annotation['dna-ssm'])" text-color="black" label="DNA-SSM">: {{ annotation["dna-ssm"] }}</q-badge></div>
+                <div><q-badge title="DNA - Copy Number Variants (CNV)" :color="getColor(annotation['dna-cnv'])" text-color="black" label="DNA-CNV">: {{ annotation["dna-cnv"] }}</q-badge></div>
+                <div><q-badge title="DNA - Structural Variants (SV)" :color="getColor(annotation['dna-sv'])" text-color="black" label="DNA-SV">: {{ annotation["dna-sv"] }}</q-badge></div>
               </div>
             </template>
           </td>
@@ -39,13 +39,13 @@
             <template v-if="indexing">
               <div class="row items-center q-gutter-x-sm">
                 <div class="u-text-key">Total: {{ general.indexing }}</div>
-                <div><q-badge title="Donors" :color="getColor(indexing.Donor)" text-color="black" label="Donor">: {{ indexing.Donor }}</q-badge></div>
-                <div><q-badge title="Images" :color="getColor(indexing.Image)" text-color="black" label="Image">: {{ indexing.Image }}</q-badge></div>
-                <div><q-badge title="Specimens" :color="getColor(indexing.Specimen)" text-color="black" label="Specimen">: {{ indexing.Specimen }}</q-badge></div>
-                <div><q-badge title="Genes" :color="getColor(indexing.Gene)" text-color="black" label="Gene">: {{ indexing.Gene }}</q-badge></div>
-                <div><q-badge title="Simple Somatic Mutations (SSM)" :color="getColor(indexing.SSM)" text-color="black" label="SSM">: {{ indexing.SSM }}</q-badge></div>
-                <div><q-badge title="Copy Number Variants (CNV)" :color="getColor(indexing.CNV)" text-color="black" label="CNV">: {{ indexing.CNV }}</q-badge></div>
-                <div><q-badge title="Structural Variants (SV)" :color="getColor(indexing.SV)" text-color="black" label="SV">: {{ indexing.SV }}</q-badge></div>
+                <div><q-badge title="Donors index" :color="getColor(indexing.Donor)" text-color="black" label="Donor">: {{ indexing.Donor }}</q-badge></div>
+                <div><q-badge title="Images index" :color="getColor(indexing.Image)" text-color="black" label="Image">: {{ indexing.Image }}</q-badge></div>
+                <div><q-badge title="Specimens index" :color="getColor(indexing.Specimen)" text-color="black" label="Specimen">: {{ indexing.Specimen }}</q-badge></div>
+                <div><q-badge title="Genes index" :color="getColor(indexing.Gene)" text-color="black" label="Gene">: {{ indexing.Gene }}</q-badge></div>
+                <div><q-badge title="Variants index - Simple Somatic Mutations (SSM)" :color="getColor(indexing.SSM)" text-color="black" label="SSM">: {{ indexing.SSM }}</q-badge></div>
+                <div><q-badge title="Variants index - Copy Number Variants (CNV)" :color="getColor(indexing.CNV)" text-color="black" label="CNV">: {{ indexing.CNV }}</q-badge></div>
+                <div><q-badge title="Variants index - Structural Variants (SV)" :color="getColor(indexing.SV)" text-color="black" label="SV">: {{ indexing.SV }}</q-badge></div>
               </div>
             </template>
           </td>
@@ -92,7 +92,7 @@ export default {
       this.general = await api.getGeneralStats();
       this.submission = await api.getSubmissionStats();
       this.annotation = await api.getAnnotationStats();
-      this.indexing = await api.getIndexingStats();
+      this.indexing = await api.getIndexingStats();      
     }
   },
 }
