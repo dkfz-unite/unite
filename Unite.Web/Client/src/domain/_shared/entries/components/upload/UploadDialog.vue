67122d74cb3fd370ff5a59f9a9114ffc895c3094
vulasau <valiantsin.ulasau@gmail.com>
2024-11-27 10:13:39 +0100

    Update UploadDialog.vue

--- Unite.Web/Client/src/domain/_shared/entries/components/upload/UploadDialog.vue
+++ Unite.Web/Client/src/domain/_shared/entries/components/upload/UploadDialog.vue
@@ -10,95 +10,116 @@
         <div class="text-h6">{{ subject }}</div>
       </q-card-section>
 
-      <q-card-section class="q-gutter-y-sm" v-if="showControls">
-        <div class="row q-gutter-x-sm">
-          <div class="text-body1">Type:</div>
-          <q-radio v-model="fileType" val="json" label="JSON" dense />
-          <q-radio v-model="fileType" val="tsv" label="TSV" dense />
-        </div>
-
-        <div class="row">
-          <div v-if="fileType === 'json'" class="col">
-            Download json file <a :href="templatePathJson" :download="`template-${getFileName(templatePathJson)}`" class="u-link">template</a>
+      <!-- Sumission process -->
+      <template v-if="!submission">
+        <q-card-section class="q-gutter-y-sm">
+          <div class="row q-gutter-x-sm">
+            <div class="text-body1">Type:</div>
+            <q-radio v-model="fileType" val="json" label="JSON" dense />
+            <q-radio v-model="fileType" val="tsv" label="TSV" dense />
           </div>
-          <div v-if="fileType === 'tsv'" class="col">
-            Download tsv file <a :href="templatePathTsv" :download="`template-${getFileName(templatePathTsv)}`" class="u-link">template</a>
+
+          <div class="row">
+            <div v-if="fileType === 'json'" class="col">
+              Download json file <a :href="templatePathJson" :download="`template-${getFileName(templatePathJson)}`" class="u-link">template</a>
+            </div>
+            <div v-if="fileType === 'tsv'" class="col">
+              Download tsv file <a :href="templatePathTsv" :download="`template-${getFileName(templatePathTsv)}`" class="u-link">template</a>
+            </div>
           </div>
-        </div>
 
-        <div class="row">
-          <div class="col">
-            Fields description can be found <a :href="modelDocs" target="_blank" class="u-link">here</a>
+          <div class="row">
+            <div class="col">
+              Fields description can be found <a :href="modelDocs" target="_blank" class="u-link">here</a>
+            </div>
           </div>
-        </div>
 
-        <q-file
-          class="ellipsis"
-          ref="uploadInput"
-          label="File"
-          v-model="file.value"
-          :rules="file.rules"
-          clearable
-          square outlined dense>
-          <template #prepend>
-            <q-icon name="las la-paperclip" size="sm" />
-          </template>
-        </q-file>
+          <q-file
+            class="ellipsis"
+            ref="uploadInput"
+            label="File"
+            v-model="file.value"
+            :rules="file.rules"
+            clearable
+            square outlined dense>
+            <template #prepend>
+              <q-icon name="las la-paperclip" size="sm" />
+            </template>
+          </q-file>
 
-        <div v-if="error?.status == 400" class="row">
-          <div class="col">
-            <div class="text-negative">There are validation errors:</div>
-            <q-scroll-area style="height: 120px; border: 1px solid lightgrey">
-              <div v-for="fieldError in Object.entries(error.data)" class="row q-pa-xs">
-                <div class="col">
-                  <div class="row text-weight-bold">{{ fieldError[0] }}</div>
-                  <div class="row" v-for="fieldErrorMessage in fieldError[1]"> {{ fieldErrorMessage }}</div>
+          <div v-if="error?.status == 400" class="row">
+            <div class="col">
+              <div class="text-negative">There are validation errors:</div>
+              <q-scroll-area style="height: 120px; border: 1px solid lightgrey">
+                <div v-for="fieldError in Object.entries(error.data)" class="row q-pa-xs">
+                  <div class="col">
+                    <div class="row text-weight-bold">{{ fieldError[0] }}</div>
+                    <div class="row" v-for="fieldErrorMessage in fieldError[1]"> {{ fieldErrorMessage }}</div>
+                  </div>
                 </div>
-              </div>
-            </q-scroll-area>
+              </q-scroll-area>
+            </div>
           </div>
-        </div>
-        <div v-else-if="error" class="row">
-          <div class="col">
-            <span class="text-negative">{{ error.data }}</span>
+          <div v-else-if="error" class="row">
+            <div class="col">
+              <span class="text-negative">{{ error.data }}</span>
+            </div>
           </div>
-        </div>
-      </q-card-section>
+        </q-card-section>
 
-      <q-card-actions align="right" class="text-primary" v-if="showControls">
-        <q-btn
-          label="Cancel"
-          dense flat no-caps
-          v-close-popup
-        />
+        <q-card-actions align="right" class="text-primary">
+          <q-btn
+            label="Cancel"
+            dense flat no-caps
+            v-close-popup
+          />
 
-        <q-btn
-          label="Apply"
-          :disable="!canApply"
-          @click="onApply"
-          dense flat no-caps
-        />
-      </q-card-actions>
+          <q-btn
+            label="Apply"
+            :disable="!canApply"
+            @click="onApply"
+            dense flat no-caps
+          />
+        </q-card-actions>
+      </template>
 
-      <q-card-section v-if="submissionTaskId">
-        <div> Submission Id : <b>{{ this.submissionTaskId }}</b><br> Please use this submission number to track status</div>
-      </q-card-section>
+      <!-- Submission result -->
+      <template v-else>
+        <q-card-section>
+          <div class="text-positive">Your data has been successfuly submitted.</div>
+          <div>Sumbission number: <b>{{ this.submission }}</b>.</div>
+          <div>You can use this number to <u-link :to="SubmissionsSettings.domain">track</u-link> you submission status.</div>
+        </q-card-section>
 
-      <q-card-actions align="right" class="text-primary" v-if="submissionTaskId">
-        <q-btn
-          label="Close"
-          dense flat no-caps
-          v-close-popup/>
-      </q-card-actions>
-
+        <q-card-actions align="right" class="text-primary">
+          <q-btn
+            label="Close"
+            dense flat no-caps
+            v-close-popup/>
+        </q-card-actions>
+      </template>
     </q-card>
   </q-dialog>
 </template>
 
 <script>
+import ULink from "@/_shared/components/base/Link.vue";
+import SubmissionsSettings from "@/domain/submissions/settings";
+
+
 const defaultFileType = "json";
 
 export default {
+  components: {
+    ULink
+  },
+
+  setup() {
+    return {
+      SubmissionsSettings
+    }
+  },
+
   props: {
     modelValue: {
       type: Boolean,
@@ -141,8 +162,7 @@ export default {
         ]
       },
       error: null,
-      submissionTaskId: null,
-      showControls: true
+      submission: null
     };
   },
 
@@ -173,12 +193,11 @@ export default {
   methods: {
     async onApply() {
       this.error = null;
-      this.submissionTaskId = null;
+      this.submission = null;
 
       try {
-          this.submissionTaskId = await this.uploadMethod(this.file.value, this.fileType);
-          this.showControls = false;
-        } catch (error) {
+        this.submission = await this.uploadMethod(this.file.value, this.fileType);
+      } catch (error) {
         this.error = error;
         this.canApply = false;
         this.notifyError(`Couldn't upload ${this.subjectLower}`);
@@ -188,8 +207,7 @@ export default {
     async onClose() {
       this.file.value = null;
       this.fileType = defaultFileType;
-      this.submissionTaskId = null;
-      this.showControls = true;
+      this.submission = null;
     },
 
     async fileIsNotEmpty(file) {
