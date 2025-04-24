22af8f9c393aa435b9ea25210426747bc5e031b5
vulasau <valiantsin.ulasau@gmail.com>
2025-04-24 10:40:08 +0200

    Updated filter fields. Enabled typescript.

--- Unite.Web/Client/src/domain/donors/models/filters/donor-filters-criteria.js
+++ Unite.Web/Client/src/domain/donors/models/filters/donor-filters-criteria.js
@@ -23,6 +23,8 @@ export default class DonorFiltersCriteria {
     hasCnvs = null;
     hasSvs = null;
     hasMeth = null;
+    hasExp = null;
+    hasExpSc = null;
 
     get numberOfFilters() {
         let number = 0;
@@ -49,8 +51,8 @@ export default class DonorFiltersCriteria {
         number += this.hasCnvs != null ? 1 : 0;
         number += this.hasSvs != null ? 1 : 0;
         number += this.hasMeth != null ? 1 : 0;
-        number += this.hasGeneExp != null ? 1 : 0;
-        number += this.hasGeneExpSc != null ? 1 : 0;
+        number += this.hasExp != null ? 1 : 0;
+        number += this.hasExpSc != null ? 1 : 0;
 
         return number;
     }
@@ -75,8 +77,8 @@ export default class DonorFiltersCriteria {
         this.hasCnvs = criteria?.hasCnvs != null ? criteria.hasCnvs : null;
         this.hasSvs = criteria?.hasSvs != null ? criteria.hasSvs : null;
         this.hasMeth = criteria?.hasMeth != null ? criteria.hasMeth : null;
-        this.hasGeneExp = criteria?.hasGeneExp != null ? criteria.hasGeneExp : null;
-        this.hasGeneExpSc = criteria?.hasGeneExpSc != null ? criteria.hasGeneExpSc : null;
+        this.hasExp = criteria?.hasExp != null ? criteria.hasExp : null;
+        this.hasExpSc = criteria?.hasExpSc != null ? criteria.hasExpSc : null;
     }
 
     sanitise() {
@@ -118,8 +120,8 @@ export default class DonorFiltersCriteria {
         this.hasCnvs = null;
         this.hasSvs = null;
         this.hasMeth = null;
-        this.hasGeneExp = null;
-        this.hasGeneExpSc = null;
+        this.hasExp = null;
+        this.hasExpSc = null;
     }
 
     clone() {
