d72733e912bf58f39e819df1dc36a0eccd0d7887
SemionV <semion.varennik@gmail.com>
2026-06-03 15:16:50 +0200

    Integrate BitECS

---
+++ Unite.Web/Client/src/_shared/components/graphics/VectorGraphics.vue
@@ -0,0 +1,31 @@
+<template>
+  <canvas ref="canvasRef" :width="width" :height="height" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+  createWorld,
+  addEntity,
+  addComponent,
+  query,
+  type World,
+} from 'bitecs'
+
+export default defineComponent({
+  props: {
+    width: {
+      type: Number,
+      default: 800,
+    },
+    height: {
+      type: Number,
+      default: 600,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
